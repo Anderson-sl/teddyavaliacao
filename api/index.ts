06aec7f0b43e8f2ad078ea4d96ff21c83dd250f5
Anderson dos Santos <anderson.leonardo@smartnx.com>
2024-10-23 21:50:27 -0300

    Criado arquivo de index de inicialização da API

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,19 @@
-//Project Start
+import dotenv from 'dotenv';
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+dotenv.config();
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const {
+    SERVER_PORT: serverPort = 3000,
+} = process.env;
+
+const server = http.createServer(app);
+
+server.listen(serverPort, () => {
+    console.log(`API Running in port ${serverPort}`);
+});
