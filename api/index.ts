6b6a7bcbe4840d198739758a8b436995a452479c
Anderson dos Santos <anderson.leonardo@smartnx.com>
2024-10-24 23:49:09 -0300

    Adicionado class Server para inicialização da API

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,31 @@
 import dotenv from 'dotenv';
 import http from 'http';
 import express from 'express';
+import { Express } from 'express-serve-static-core';
 import bodyParser from 'body-parser';
-
+import userRoutes from './src/routes/users';
 dotenv.config();
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 const {
     SERVER_PORT: serverPort = 3000,
 } = process.env;
 
-const server = http.createServer(app);
+class Server {
+    private app: Express = express();
+    private port: number = 3000;
+    constructor() {
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(bodyParser.json());
+        this.app.use('/user', userRoutes);
+        this.port = Number(serverPort);
+    }
 
-server.listen(serverPort, () => {
-    console.log(`API Running in port ${serverPort}`);
-});
+    start(){
+        http.createServer(this.app)
+            .listen(this.port, () => {
+                console.log(`API Running in port ${this.port}`);
+            });
+    }
+}
+
+const server = new Server();
+server.start();
